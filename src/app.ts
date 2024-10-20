92467096eb23307b173264a69df0dc3c6596c604
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-10-20 17:48:49 +0200

    server start

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import http from 'node:http';
-import { handleRequest } from 'routes/userRoutes';
+import { handleRequest } from './routes/userRoutes';
 
 const host = 'localhost';
 const port = 4000;

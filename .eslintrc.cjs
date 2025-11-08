e33b64a20fafed930d4e2c1c48b0b92dc5146b66
Aliaksei-Varabyou <aleksey.vorobyov@gmail.com>
2025-11-08 23:22:11 +0100

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // включает prettier как правило ESLint
+  ],
+  rules: {
+    'prettier/prettier': 'error', // ошибки prettier будут подсвечиваться ESLint
+    'no-console': 'off', // можно использовать console.log в Node
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
